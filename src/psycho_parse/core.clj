3b39fc14a2dda707466c9776103aa086e243c622
pce1991 <patrick.collin.eye@gmail.com>
2014-11-08 22:46:45 -0600

    Added open-nlp requirements

--- src/psycho_parse/core.clj
+++ src/psycho_parse/core.clj
@@ -1,10 +1,20 @@
 (ns psycho-parse.core
+  (:require [opennlp.nlp :refer :all]
+            [clj-wordnet.core :refer :all])
   (:gen-class))
 
 ;;; use opennlp to get a parse of the sentence, then go to each word, get its
 ;;; WN entry, and then find its hypernyms and replace each word with it to
 ;;; the requested depth, so you could get "the dog ran" as "the canine moved"
 ;;; or "the animal acted" 
+
+(def get-sentences (make-sentence-detector "models/ent-sent.bin"))
+(def tokenize (make-tokenizer "models/en-token.bin"))
+(def pos-tag (mage-pos-tagger "models/en-pos-maxent.bin"))
+(def chunker (make-treebank-chunker "models/en-chunker.bin"))
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
