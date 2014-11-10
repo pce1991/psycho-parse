9e1e6c09a14d5fa28032b70070eb65ab9698e0ac
pce1991 <patrick.collin.eye@gmail.com>
2014-11-10 00:09:55 -0600

    Begun skeleton

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject psycho-parse "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :jvm-opts ["-Xmx512m"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-wordnet "0.1.1-SNAPSHOT"]]
+                 [clj-wordnet "0.1.1-SNAPSHOT"]
+                 [clojure-opennlp "0.3.2"]]
   :main ^:skip-aot psycho-parse.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/psycho_parse/core.clj
+++ src/psycho_parse/core.clj
@@ -1,5 +1,6 @@
 (ns psycho-parse.core
   (:require [opennlp.nlp :refer :all]
+            [opennlp.treebank :refer :all]
             [clj-wordnet.core :refer :all])
   (:gen-class))
 
@@ -8,15 +9,24 @@
 ;;; the requested depth, so you could get "the dog ran" as "the canine moved"
 ;;; or "the animal acted" 
 
-(def get-sentences (make-sentence-detector "models/ent-sent.bin"))
-(def tokenize (make-tokenizer "models/en-token.bin"))
-(def pos-tag (mage-pos-tagger "models/en-pos-maxent.bin"))
-(def chunker (make-treebank-chunker "models/en-chunker.bin"))
+(def get-sentences (make-sentence-detector "resources/models/en-sent.bin"))
+(def tokenize (make-tokenizer "resources/models/en-token.bin"))
+(def pos-tag (make-pos-tagger "resources/models/en-pos-maxnet.bin"))
+(def chunker (make-treebank-chunker "resources/models/en-chunker.bin"))
+(def treebank-parser (make-treebank-parser
+                      "resources/models/en-parser-chunking.bin"))
+
+(defn hypernyms [word]
+  ;; recursively get the related-synsets of words, then of those
+  ;; but how do I know what sense/entry-number they are?
+  )
+
+(defn word->hypernym [word depth]
+  ;; if depth is greater than count of hypernyms then just get last.
+  )
+
+;;; hard part here is diving into the dense tree and changing values 
+(defn replace-words [treebank]
+  )
 
 
-
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
