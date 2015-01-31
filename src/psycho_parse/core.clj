c12c06b8b561be557f872183e865036ae96aeaab
pce1991 <patrick.collin.eye@gmail.com>
2015-01-30 19:30:03 -0600

    getting hypernyms as tree, unfinished

--- src/psycho_parse/core.clj
+++ src/psycho_parse/core.clj
@@ -19,50 +19,53 @@
 
 (defn disambiguate [])
 
-;; just do the basics here of getting the ENTRIES of just the toplevel
-;; hypernyms. I definetly dont want the lemmas
-;; do I know that the hypernyms of those words are the next thing up from
-;; previous ones.
-;; the related synset returns a map of synset ids which can keyed to
-;; lists of words, an example being that a certain synset has both candid
-;; and canine as lemmas
 (defn nearest-hypernyms [entry]
-  ;; recursively get the related-synsets of words, then of those
-  ;; but how do I know what sense/entry-number they are?
-  ;; worry about that later, right now just get the hypernyms
-  ;; need to call perl from here or implement algorithm
-  ;; maybe just execute a perl script
-
-  ;; i think what im actually worried about is how to know which sense
-  ;; of a synset to look up. do i know that members of a synset have the
-  ;; same hypernym
-  ;; i want to build up a tree of these, maybe just branches at synsets
-  ;; and then vectors showing the order of supremacy
-  ;; keep in mind tho that any synset could split up at any point
-  ;; so at every point I must check to see if there's a split
   (let [synsets (related-synsets entry :hypernym)
         hypernyms (map #(into [] %) (vals synsets))]
     ;; i want to build up the trees for each synset, 
     (into [] hypernyms)))
 
-
-;; THIS IS RIDICULOUS! 129646! I must be looking up too many redundant words. how do I know which to ignore? if the synset id is already in cumulative; this might not work for the branching hierarchies
-;; i want divide these up based on where they branch, but do synsets have
-;; an order so that synset of canine is less than synset of entity?
+;;its important that this not go thru so many tries as the other which
+;;added way to mayn redundant cases to hypernyms
+;; create a tree structure
+;;look at current hypernyms of a word, and then update that into the set
 (defn hypernyms [entry]
-  (loop [hypernyms (flatten (nearest-hypernyms entry))
-         cumulative []]
-    ;;just loop thru moving the front cumulative and add its hyps to hypernyms
-    (if (empty? hypernyms)
-      cumulative
-      (if (empty? (nearest-hypernyms (first hypernyms)))
-        ;; this is when we know its the end of that tree
-        (recur (rest hypernyms) (conj cumulative (first hypernyms)))
-        (recur
-         (concat (rest hypernyms)
-                 (flatten (nearest-hypernyms (first hypernyms))))
-         (conj cumulative (first hypernyms)))))))
+  (loop [hypernyms {}             ;(related-synsets entry :hypernym)
+         to-search [entry]             ;(map #(into [] %) (vals hypernyms))
+         synset-ids []]
+;   (println (map :lemma to-search))
+;    (println (count synset-ids))
+    (clojure.pprint/pprint hypernyms)
+    (if (empty? to-search)
+      hypernyms
+      (let [e (first to-search)
+            synset-id (:synset-id e)
+            ;; sometimes this may need to be reset, when we return to an
+            ;; earlier synset, but which one? if we get a synset thats
+            ;;already appeared I think we know to reset to that point
+            synset-ids (conj synset-ids synset-id)
+            hyprnms (related-synsets e :hypernym)
+            hyp-entries (flatten (vals hyprnms))
+            ]
+        (if (empty? hyprnms)
+          (recur hypernyms (rest to-search) synset-ids) ;;wrong?!
+          ;;i may need to build up a vector of synset-ids, but it couldnt just accumlate
+          ;; 
+          (recur
+           ;;right now synset is going out of bounds?
+           ;;i think i definitely need update in, its losing all the
+           ;;the early entries, keeping only synset. think of its idealform
+           ;;each id needs to hold its hyps, and also its childre
+           (assoc-in  hypernyms synset-ids hyprnms)
+                 (concat hyp-entries (rest to-search))
+                 ;;this is so it follows one path all the way before backing up.
+                 synset-ids
+                 ))
 
+        ))
+    ))
+
+;; how do i know which synset to go down?
 (defn word->hypernym [word depth]
   ;; if depth is greater than count of hypernyms then just get last.
   )
